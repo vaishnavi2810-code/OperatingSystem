6bd6f07d310999723557ea342a144c1f41f24d4e
vaishnavi2810-code <65387125+vaishnavi2810-code@users.noreply.github.com>
2021-06-06 16:28:25 +0530

    Update FCFS1.cpp

--- FCFS1.cpp
+++ FCFS1.cpp
@@ -3,13 +3,11 @@ int waiting_time(int processes[],int n,int burst_time[],int wait_time[]){
 wait_time[0]=0;
 for(int i=1;i<n;i++)
     wait_time[i]=burst_time[i-1]+wait_time[i-1];
-
 }
 int turn_around_time(int processes[],int n,int burst_time[],int wait_time[],int tat[]){
 int i;
 for(i=0;i<n;i++)
     tat[i]=burst_time[i]+wait_time[i];
-
 }
 int avg_time(int processes[],int n,int burst_time[]){
 int wait_time[n],tat[n],total_wt=0,total_tat=0;
@@ -26,11 +24,9 @@ printf("Average waiting time=%f\n ",(float)total_wt/(float)n);
 printf("Total Turn Around Time=%f\n ",(float)total_tat/(float)n);
 }
 int main(){
-
 int processes[]={1,2,3,4};
 int n =sizeof processes/sizeof processes[0];
 int burst_time[]={2,2,3,4};
 avg_time(processes,n,burst_time);
-
 }
 
